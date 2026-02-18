c5d57bff4ef79837221e202a917a8420955c6ea3
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2026-02-18 20:43:07 -0300

    feat: aula 18/02/2026

---
+++ umfg.venda.app/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using umfg.venda.app.Abstracts;
+
+namespace umfg.venda.app.ViewModels
+{
+    internal sealed class MainWindowViewModel : AbstractViewModel
+    {
+        public MainWindowViewModel() : base("UMFG - Tela Pricipal")
+        {
+        }
+    }
+}
